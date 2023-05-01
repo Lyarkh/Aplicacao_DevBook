793f5287194091b7dcaa7abe57590c235828057f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-01 11:30:36 -0300

    feat: código para criação da tabela no mysql

---
+++ api/sql/sql.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXIST devbook;
+USE devbook;
+DROP TABLE IF EXISTS usuarios;
+CREATE TABLE usuarios (
+    id int auto_increment primary key,
+    nome VARCHAR(50) NOT NULL,
+    nick VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    senha VARCHAR(20)  NOT NULL,
+    criandoEm TIMESTAMP default CURRENT_TIMESTAMP()
+)ENGINE=InnoDB;
