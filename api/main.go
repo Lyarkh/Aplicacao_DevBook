3b804e796be82f410f4ac73029b041dcd26fad35
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-01 11:51:27 -0300

    feat: criação do pacote que cria a conexão com o banco de dados

--- api/main.go
+++ api/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	config.Carregar()
-	fmt.Println("Rodando API")
 	r := router.Gerar()
 
+	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
