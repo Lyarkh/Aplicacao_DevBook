fc5176bf13addba8f346603dc8f8582cad42e0aa
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-01 15:55:36 -0300

    feat: criando repositorio de usuario para criar o usuario no db

---
+++ api/src/repositorios/usuarios.go
@@ -0,0 +1,21 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+// Usuarios representa um repositorio de usuarios
+type Usuarios struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDeUsuarios cria um repositorio de usuarios
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db: db}
+}
+
+// Criar insere um usuário no banco de dados
+func (u Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	return 0, nil
+}
