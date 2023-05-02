7d524c55a04cbb5afe3b0e5dc74afccd6fb79e49
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-02 00:08:17 -0300

    feat: criando middlewares e base para validação do usuario no uso das rotas que precisam de autenticação

--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -1,6 +1,7 @@
 package rotas
 
 import (
+	"api/src/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -18,9 +19,16 @@ type Rota struct {
 func Configurar(r *mux.Router) *mux.Router {
 	rotas := rotasUsuarios
 	rotas = append(rotas, rotaLogin)
-	
+
 	for _, rota := range rotas {
-		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+
+		if rota.RequerAutenticacao {
+			r.HandleFunc(rota.URI,
+				middlewares.Logger(middlewares.Autenticar(rota.Funcao)),
+			).Methods(rota.Metodo)
+		} else {
+			r.HandleFunc(rota.URI, middlewares.Logger(rota.Funcao)).Methods(rota.Metodo)
+		}
 	}
 
 	return r
