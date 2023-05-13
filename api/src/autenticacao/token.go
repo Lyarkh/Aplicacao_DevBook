ce72582ada4da3e733c7349735ab6d228d8de453
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-13 17:42:02 -0300

    feat: função que extrai o usuario ID

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -34,6 +35,27 @@ func ValidarToken(r *http.Request) error {
 		return nil
 	}
 	return errors.New("token inválido")
+}
+
+// Função que extrai o ID do usuario pelo TOKEN
+func ExtrairUsuarioID(r *http.Request) (uint64, error) {
+	tokenString := extrairToken(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveDeVerificacao)
+	if erro != nil {
+		return 0, erro
+	}
+
+	if permissoes, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		usuarioID, erro := strconv.ParseUint(fmt.Sprintf("%.0f", permissoes["usuarioId"]), 10, 64)
+		if erro != nil {
+			return 0, erro
+		}
+
+		return usuarioID, nil
+	}
+
+	return 0, errors.New("token inválido")
+
 }
 
 func extrairToken(r *http.Request) string {
