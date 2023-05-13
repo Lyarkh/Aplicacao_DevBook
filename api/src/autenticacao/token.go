9e8854da4d854d0d22c0c7c5897dbffdfa2e25af
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-12 21:35:00 -0300

    feat: adicionando funções de extração e retorno do token

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -2,6 +2,10 @@ package autenticacao
 
 import (
 	"api/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -16,4 +20,24 @@ func CriarToken(usuarioID uint64) (string, error) {
 	// secret token
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissoes)
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+
+
+func extrairToken(r *http.Request) string {
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+
+	return ""
+}
+
+func retornarChaveDeVerificacao(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("Metodo de assinatura inesperado ! %v", token.Header["alg"])
+	}
+
+	return config.SecretKey, nil
 }
