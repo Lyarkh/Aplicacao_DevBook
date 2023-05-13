09da0ef1d82f63584166d23c06b7a3a8bdc24579
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-13 17:52:39 -0300

    feat: adicionando verificação do ID na função de deletar o usuario
    permitindo apenas deletar o próprio usuario e não o usuario com outro ID

--- api/src/controllers/usuario.go
+++ api/src/controllers/usuario.go
@@ -164,6 +164,17 @@ func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	usuarioIDNoToken, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if usuarioID != usuarioIDNoToken {
+		respostas.Erro(w, http.StatusForbidden, errors.New("não é possivel deletar um usuário que não seja o seu"))
+		return
+	}
+
 	db, erro := banco.Conectar()
 	if erro != nil {
 		respostas.Erro(w, http.StatusInternalServerError, erro)
