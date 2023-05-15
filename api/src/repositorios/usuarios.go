692fc932cd70c16cc7449cf089123192b63eac0f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-15 15:05:47 -0300

    feat: adicionando rota que faz um usuário seguir outro usuário

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -159,3 +159,20 @@ func (repositorio Usuarios) BuscarPorEmail(email string) (modelos.Usuario, error
 	}
 	return usuario, nil
 }
+
+func (repositorio Usuarios) Seguir(usuarioID, seguidorID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"INSERT IGNORE INTO seguidores (usuario_id, seguidor_id) VALUES (?, ?)",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(usuarioID, seguidorID); erro != nil {
+		return erro
+	}
+
+	return nil
+
+}
