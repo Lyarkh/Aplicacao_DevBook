4e4b6090088690f4551084b46f92fd4d190cc35a
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-15 14:40:12 -0300

    feat: criação da tabela seguidores

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -9,3 +9,11 @@ CREATE TABLE usuarios (
     senha VARCHAR(100)  NOT NULL,
     criandoEm TIMESTAMP default CURRENT_TIMESTAMP()
 )ENGINE=InnoDB;
+
+drop table if EXISTS seguidores
+CREATE TABLE seguidores (
+    usuario_id INT NOT NULL, FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+    seguidor_id INT NOT NULL, FOREIGN KEY (seguidor_id) REFERENCES usuarios(id) ON DELETE CASCADE,
+
+    PRIMARY KEY(usuario_id, seguidor_id)
+)ENGINE=InnoDB;
