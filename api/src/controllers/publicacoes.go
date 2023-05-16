f9192b1db991aed4366da28b3179fe511836a77e
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-16 20:55:33 -0300

    feat: declarando controllers para as rotas criadas

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao cria um publicacao no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacoes buscar todas as publicacoes no banco de dados
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// BuscarPublicacao busca um publicação específicada pelo ID
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// AtualizarPublicacao atualiza uma publicacao no banco de dados
+func AtualizarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// DeletarPublicacao deleta uma publicacao no banco de dados
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
