7a4e9f5cef26d5adc93ff15c2c045643102989f9
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-20 18:40:32 -0300

    feat: controller responsável por tratar a requisição para trazer todas as publicações de um determinado usuario

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -213,3 +213,30 @@ func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 	respostas.JSON(w, http.StatusNoContent, nil)
 }
+
+
+// BuscarPublicacoesPorUsuario traz todas as publicações de um determinado usuario
+func BuscarPublicacoesPorUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.BuscarPorUsuario(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacoes)
+}
