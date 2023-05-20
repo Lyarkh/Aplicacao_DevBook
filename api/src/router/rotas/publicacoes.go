fe1cfbf20cd1c60b38cfe06aa10a7ccd78d5c3e9
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-20 18:35:46 -0300

    feat: adicionando declaração de rota para buscar as publicações de um usuario especificado pelo ID

--- api/src/router/rotas/publicacoes.go
+++ api/src/router/rotas/publicacoes.go
@@ -37,4 +37,10 @@ var rotasPuplicacoes = []Rota{
 		Funcao:             controllers.DeletarPublicacao,
 		RequerAutenticacao: true,
 	},
+	{
+		URI:                "usuarios/{usuarioId}/publicacoes",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarPublicacao,
+		RequerAutenticacao: true,
+	},
 }
