aa69559ae7348048d9e498bc55c1a21f88b979a7
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-19 22:03:01 -0300

    feat: função responsável por fazer a conexão e deletar a publicação do banco de dados

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -126,3 +126,21 @@ func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao modelos
 
 	return nil
 }
+
+func (repositorio Publicacoes) Deletar(publicacaoID uint64) error {
+	statement, erro := repositorio.db.Prepare(
+		"DELETE FROM publicacoes WHERE id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+
+}
