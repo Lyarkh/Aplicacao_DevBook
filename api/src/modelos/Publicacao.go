ed04b93c8c16923e60ad7334b19da09f5ece7dde
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-19 21:13:26 -0300

    funões que faz a validação e formata o modelo Publicacao

--- api/src/modelos/Publicacao.go
+++ api/src/modelos/Publicacao.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 // Publicacao representa um publicacao feita por um usuário
 type Publicacao struct {
@@ -13,12 +17,29 @@ type Publicacao struct {
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
 }
 
-
-func (publicacao Publicacao) Preparar() error {
+func (publicacao *Publicacao) Preparar() error {
 	if erro := publicacao.validar(); erro != nil {
 		return erro
 	}
 
 	publicacao.formatar()
 	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O titulo é obrigatório e não pode estar em branco")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O titulo é obrigatório e não pode estar em branco")
+
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }
