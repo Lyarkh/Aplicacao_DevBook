d377dbc60875b15fe6a72600837e4770856535b7
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-19 21:32:39 -0300

    feat: base do controller que trata a requisição do usuario

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -61,7 +61,27 @@ func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 
 // BuscarPublicacoes buscar todas as publicacoes no banco de dados
 func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDePublicacoes(db)
+	publicacoes, erro := repositorio.Buscar(usuarioID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacoes)
 }
 
 // BuscarPublicacao busca um publicação específicada pelo ID
