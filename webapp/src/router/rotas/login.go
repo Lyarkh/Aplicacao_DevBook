b83e2ff19f6136b00cfce8065f8ccf8a7f0b2825
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-23 20:18:23 -0300

    feat: criação do package rotas
    criando struct de Rota, configuração do router e primeiras rotas referente ao Login

---
+++ webapp/src/router/rotas/login.go
@@ -0,0 +1,18 @@
+package rotas
+
+import "net/http"
+
+var rotasLogin = []Rota{
+	{
+		URI:                "/",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarTelaDeLogin,
+		RequerAutenticacao: false,
+	},
+	{
+		URI:                "/login",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.CarregarTelaDeLogin,
+		RequerAutenticacao: false,
+	},
+}
