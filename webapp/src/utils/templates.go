ba0ef8dc3acaf7e7e04a75e13cd9627b83d5f07d
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-24 21:25:58 -0300

    feat: função que carrega os templates html

---
+++ webapp/src/utils/templates.go
@@ -0,0 +1,12 @@
+package utils
+
+import "text/template"
+
+var templates *template.Template
+
+// CarregarTemplates insere os templates html na variável templates
+func CarregarTemplates() {
+	templates = template.Must(template.ParseGlob("views/*.html"))
+}
+
+
