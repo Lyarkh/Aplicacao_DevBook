9ee41476cb41d6d5adbabeeaea72e1a2488c4cc2
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-05-31 10:22:21 -0300

    refactor: modificando os valores das variáveis utilizando as variaveis de ambiente criadas

--- webapp/main.go
+++ webapp/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"webapp/src/config"
 	"webapp/src/router"
 	"webapp/src/utils"
 )
 
 func main() {
+	config.Carregar()
 	utils.CarregarTemplates()
 	r := router.Gerar()
 
-	fmt.Println("Rodando webApp!")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	fmt.Printf("Rodando webApp na porta %d!\n", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
