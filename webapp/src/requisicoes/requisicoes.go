19b2df320cc7679a898d04f5830cc7475e13a57f
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-01 20:36:53 -0300

    feat: lendo o cookie e fazendo a requisição com o token no header

--- webapp/src/requisicoes/requisicoes.go
+++ webapp/src/requisicoes/requisicoes.go
@@ -3,6 +3,7 @@ package requisicoes
 import (
 	"io"
 	"net/http"
+	"webapp/src/cookies"
 )
 
 // FazerRequisicaoComAutenticacao é utilizado para colocar o token na requisição
@@ -12,5 +13,14 @@ func FazerRequisicaoComAutenticacao(r *http.Request, metodo, url string, dados i
 		return nil, erro
 	}
 
-	
+	cookie, _ := cookies.Ler(r)
+	request.Header.Add("Authorization", "Bearer "+ cookie["token"])
+
+	client := &http.Client{}
+	response, erro := client.Do(request)
+	if erro != nil {
+		return nil, erro
+	}
+
+	return response, nil
 }
