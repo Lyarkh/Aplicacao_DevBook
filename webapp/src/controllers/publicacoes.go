f390fe73be0a3cd6fefe83cbac427131f15cca85
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-12 11:47:12 -0300

    fix: correção bug chamada respostas

--- webapp/src/controllers/publicacoes.go
+++ webapp/src/controllers/publicacoes.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
@@ -8,18 +10,17 @@ import (
 	"webapp/src/respostas"
 )
 
-
 // CriarPublicacao chama a API para cadastrar uma publicação no banco de dados
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	publicacao, erro := json.Marshal(map[string]string{
-		"titulo": r.FormValue("titulo"),
+		"titulo":   r.FormValue("titulo"),
 		"conteudo": r.FormValue("conteudo"),
 	})
 
 	if erro != nil {
-		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI({Erro: erro.Error()}))
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
 	}
 
 	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
