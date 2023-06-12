da60abf6f53e54ef80000c067102ec92c58ea0aa
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-12 12:16:56 -0300

    fix: corrigindo reponse JSON caso seja enviado um corpo de resposta vazio

--- webapp/src/respostas/respostas.go
+++ webapp/src/respostas/respostas.go
@@ -14,11 +14,12 @@ type ErroAPI struct {
 // JSON retorna uma resposta em formato JSON para a requisição
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-
 	w.WriteHeader(statusCode)
 
-	if erro := json.NewEncoder(w).Encode(dados); erro != nil {
-		log.Fatal(erro)
+	if dados != nil {
+		if erro := json.NewEncoder(w).Encode(dados); erro != nil {
+			log.Fatal(erro)
+		}
 	}
 }
 
