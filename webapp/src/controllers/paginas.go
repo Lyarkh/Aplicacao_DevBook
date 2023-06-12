74a977004bc17a1171b33cf68c44c288bf8091b3
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-12 11:56:01 -0300

    feat: lendo cookies para enviar o usario ID para o template para validar se a publicação é o do usuario que está logado

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/modelos"
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
@@ -43,5 +45,13 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	utils.ExecutarTemplate(w, "home.html", publicacoes)
+	cookie, _ := cookies.Ler()
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+	utils.ExecutarTemplate(w, "home.html", struct {
+		Publicacoes []modelos.Publicacao
+		UsuarioID   uint64
+	}{
+		Publicacoes: publicacoes,
+		UsuarioID:   usuarioID,
+	})
 }
