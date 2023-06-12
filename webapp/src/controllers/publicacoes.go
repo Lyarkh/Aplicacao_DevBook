a7319bd50016f963751f8eda917b71b1582eab18
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-12 11:31:57 -0300

    feat: base do controller de publicação para criação de uma publicação

---
+++ webapp/src/controllers/publicacoes.go
@@ -0,0 +1,8 @@
+package controllers
+
+import "net/http"
+
+// CriarPublicacao chama a API para cadastrar uma publicação no banco de dados
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	
+}
