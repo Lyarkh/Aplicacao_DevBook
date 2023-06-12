b9a2d0ef03c696ac26e0b859c7ce294e8b9e0909
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-12 11:43:30 -0300

    feat: controller responsável por criar a publucação chamando a API

--- webapp/src/controllers/publicacoes.go
+++ webapp/src/controllers/publicacoes.go
@@ -4,5 +4,29 @@ import "net/http"
 
 // CriarPublicacao chama a API para cadastrar uma publicação no banco de dados
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
-	
+	r.ParseForm()
+
+	publicacao, erro := json.Marshal(map[string]string{
+		"titulo": r.FormValue("titulo"),
+		"conteudo": r.FormValue("conteudo"),
+	})
+
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI({Erro: erro.Error()}))
+	}
+
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, bytes.NewBuffer(publicacao))
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeError(w, response)
+		return
+	}
+	respostas.JSON(w, response.StatusCode, nil)
 }
