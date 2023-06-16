b8a0afd17277b729aaa6dc011c7fb29cf82d942e
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-16 19:29:47 -0300

    feat: fazendo a leitura de parâmetros da requisição feita para a página

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -55,3 +55,14 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 		UsuarioID:   usuarioID,
 	})
 }
+
+
+// CarregarPaginaDeEdicaoDePublicacao carrega a pagina de edição de publicação
+func CarregarPaginaDeEdicaoDePublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+}
