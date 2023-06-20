c285dd2f82f01b7b28aecc3157b293974f1b39cf
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-20 10:06:55 -0300

    style: formatação de código

--- webapp/src/modelos/Usuario.go
+++ webapp/src/modelos/Usuario.go
@@ -1,15 +1,17 @@
 package modelos
 
-import "time"
+import (
+	"time"
+)
 
 // Usuario representa uma pessoa utilizando a aplicação
 type Usuario struct {
-	ID          uint64    `json:"id"`
-	Nome        string    `json:"nome"`
-	Email       string    `json:"email"`
-	Nick        string    `json:"nick"`
-	CriadoEm    time.Time `json:"criadoEm"`
-	Seguidores  []Usuario `json:"seguidores"`
-	Seguindores []Usuario `json:"seguindo"`
+	ID          uint64       `json:"id"`
+	Nome        string       `json:"nome"`
+	Email       string       `json:"email"`
+	Nick        string       `json:"nick"`
+	CriadoEm    time.Time    `json:"criadoEm"`
+	Seguidores  []Usuario    `json:"seguidores"`
+	Seguindores []Usuario    `json:"seguindo"`
 	Publicacoes []Publicacao `json:"publicacoes"`
 }
