0fb47e47dd5269b7a670d6338d0896cb22eb03bd
Lyarkh <lcemanuel.emanuel@gmail.com>
2023-06-20 11:18:59 -0300

    feat: controller responsável por tratar e seguir um usuario no webapp

--- webapp/src/controllers/usuarios.go
+++ webapp/src/controllers/usuarios.go
@@ -67,3 +67,28 @@ func PararDeSeguirUsuario(w http.ResponseWriter, r *http.Request) {
 	}
 	respostas.JSON(w, response.StatusCode, nil)
 }
+
+// SeguirUsuario chama a API para seguir um usuário
+func SeguirUsuario(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	usuarioID, erro := strconv.ParseUint(parametros["usuarioId"], 10, 64)
+	if erro != nil {
+		respostas.JSON(w, http.StatusBadRequest, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/usuarios/%d/seguir", config.APIURL, usuarioID)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodPost, url, nil)
+	if erro != nil {
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeError(w, response)
+		return
+	}
+	respostas.JSON(w, response.StatusCode, nil)
+}
